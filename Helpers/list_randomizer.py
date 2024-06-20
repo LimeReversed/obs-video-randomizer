a12a3f4c7f15a12e5086b06e94e3a106df4b21e3
Lime Fortytwo <lime42@pm.me>
2024-06-20 17:53:43 +0200

    Added merge and extend to list_randomizer

--- Helpers/list_randomizer.py
+++ Helpers/list_randomizer.py
@@ -4,9 +4,9 @@ import json
 
 class ListRandomizer(object):
 
-    def __init__(self, list):
-        self.list = list
-        self._current_last_index = len(list) - 1
+    def __init__(self, new_list):
+        self._list = new_list
+        self._current_last_index = len(new_list) - 1
 
     def to_json(self):
         json_string = json.dumps(self, default=lambda obj: self.__dict__, indent=4)
@@ -19,17 +19,31 @@ class ListRandomizer(object):
         return new_list_randomizer
 
     def get_next_element(self):
+        if self._current_last_index < 0:
+            self._current_last_index = len(self._list) - 1
+
         next_index = random.randint(0, self._current_last_index)
-        next_element = self.list[next_index]
+        next_element = self._list[next_index]
 
         # Put the element at the end of the array
-        tmp = self.list[next_index]
-        self.list[next_index] = self.list[self._current_last_index]
-        self.list[self._current_last_index] = tmp
-
-        if self._current_last_index - 1 >= 0:
-            self._current_last_index -= 1
-        else:
-            self._current_last_index = len(self.list) - 1
+        tmp = self._list[next_index]
+        self._list[next_index] = self._list[self._current_last_index]
+        self._list[self._current_last_index] = tmp
+        self._current_last_index -= 1
 
         return next_element
+
+    def merge(self, new_list_randomizer: "ListRandomizer"):
+
+        used = new_list_randomizer._list[new_list_randomizer._current_last_index:len(new_list_randomizer._list)]
+        not_used = new_list_randomizer._list[0:new_list_randomizer._current_last_index + 1]
+        self.extend(not_used, False)
+        self.extend(used, True)
+
+    def extend(self, new_list: list, mark_as_used: bool):
+
+        if mark_as_used:
+            self._list.extend(new_list)
+        else:
+            new_list.extend(self._list)
+            self._current_last_index += len(new_list)
